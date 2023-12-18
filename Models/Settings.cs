cf62d82cbe401ee4ee0912cf1c8ffdb3b7d40e12
Kirill <kijufe@ttu.ee>
2023-12-18 21:36:38 +0200

    previous commit

---
+++ Models/Settings.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml.Serialization;
+
+namespace Models;
+
+[XmlRoot(ElementName="settings")]
+public class Settings
+{
+    [XmlAttribute(AttributeName="workingDirectoryPath")]
+    public string WorkingDirectoryPath { get; set; }
+}
